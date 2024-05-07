d36444b2b5f593f222f79213946b8f56f28316a3
name <wmoon0024@gmail.com>
2024-05-07 17:38:10 +0900

    settings updated

--- oneLabProject/settings.py
+++ oneLabProject/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-m&1gjc)a!0m&t4-skkigdplzw%e&f)5j@ksztwce%#bqaksqs)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '13.125.243.91', 'one-lab.world']
 
 
